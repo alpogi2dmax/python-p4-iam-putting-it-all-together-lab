4170a3880ca47e56c52968ad4c9d257a8be8fb20
Al Cerdan Lico <alcerdan@yahoo.com>
2025-01-08 15:12:29 -0800

    complete python iam putting it all together lab

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, make_response, jsonify
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
@@ -8,19 +8,116 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
 
+    def post(self):
+        try:
+            json = request.get_json()
+            new_user = User(
+                username = json['username'],
+                image_url = json['image_url'],
+                bio = json['bio']
+            )
+            new_user.password_hash = json['password']
+            db.session.add(new_user)
+            db.session.commit()
+            session['user_id'] = new_user.id
+            response_body = new_user.to_dict(only=('id', 'username', 'image_url', 'bio'))
+            return make_response(response_body, 201)
+        except:
+            response_body = {
+                "error": "error"
+            }
+            return make_response(response_body, 422)
+
+    
+
+
+       
 class CheckSession(Resource):
-    pass
+    
+    def get(self):
+
+        user_id = session['user_id']
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            response_body = user.to_dict(only=('id', 'username', 'image_url', 'bio'))
+            return make_response(response_body, 200)
+        else:
+            response_body = {
+                "error": "Please log in!"
+            }
+        return make_response(response_body, 401)   
 
 class Login(Resource):
-    pass
+    
+    def post(self):
+           json = request.get_json()
+           username = json['username']
+           password = json['password']
+
+           user = User.query.filter(User.username == username).first()
+
+           if user and user.authenticate(password):
+               session['user_id'] = user.id
+               response_body = user.to_dict(only=('id', 'username', 'image_url', 'bio'))
+               return make_response(response_body, 200)
+           else:
+               response_body = {
+                   "error": "Invalid username or password"
+               }
+               return make_response(response_body, 401)
 
 class Logout(Resource):
-    pass
+    
+    def delete(self):
+        user_id = session['user_id']
+        if user_id:
+            session['user_id'] = None
+            return {}, 204
+        else:
+            return {"error": "error"}, 401
 
 class RecipeIndex(Resource):
-    pass
+    
+    def get(self):
+        user_id = session['user_id']
+        if user_id:
+            recipes = [recipe.to_dict(only=('title', 'instructions', 'minutes_to_complete', 'user_id')) for recipe in Recipe.query.all()]
+            return make_response(recipes, 200)
+        else:
+            response_body = {
+                "error": "Unauthorized access. Please log in to view recipes."
+            }
+            return make_response(response_body, 401)
+        
+    def post(self):
+
+        user_id = session['user_id']
+        try:
+            if user_id:
+                json = request.get_json()
+                new_recipe = Recipe(
+                    title = json['title'],
+                    instructions = json['instructions'],
+                    minutes_to_complete = json['minutes_to_complete'],
+                    user_id = user_id
+                )
+                db.session.add(new_recipe)
+                db.session.commit()
+                response_body = new_recipe.to_dict(only=('title', 'instructions', 'minutes_to_complete', 'user_id'))
+                return make_response(response_body, 201)
+            else:
+                response_body = {
+                    "error", "Not Logged In"
+                }
+                return make_response(response_body, 401)
+        except ValueError as ve:
+               response_body = {
+                   "error": str(ve)
+               }
+               return make_response(response_body, 422)
+
+
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
---
+++ server/migrations/versions/22fd66730347_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 22fd66730347
+Revises: 
+Create Date: 2025-01-07 21:53:35.697172
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '22fd66730347'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
